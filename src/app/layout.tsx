d24380648d6d2d87e42f3af4405d97336cb95161
nijil <nmnijilkhan@gmail.com>
2026-02-18 20:37:20 +0530

    added google tag for verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <meta name="google-site-verification" content="ANOhiFuO7uZy1rcMmknjYDZXTSb0J3pCGSfPecSvWgg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet" />
